f523364fc16ce67f05c06b0fad322c90120ee4d6
Vitaly <onebeattrue@gmail.com>
2023-08-05 17:43:24 +0300

    Теперь поток выполняется параллельно основному UI потоку(main thread/ui thread)

--- app/src/main/java/edu/mirea/onebeattrue/multitask/MyThread.kt
+++ app/src/main/java/edu/mirea/onebeattrue/multitask/MyThread.kt
@@ -2,6 +2,10 @@ package edu.mirea.onebeattrue.multitask
 
 class MyThread {
     fun doWork() {
-        Thread.sleep(15000)
+        Thread(object : Runnable {
+            override fun run() {
+                Thread.sleep(15000)
+            }
+        }).start()
     }
 }
