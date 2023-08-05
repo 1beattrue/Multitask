edf13fb5380c198374b1972ddcb01b75a71dd387
Vitaly <onebeattrue@gmail.com>
2023-08-05 17:46:02 +0300

    Немного упростили реализацию, заменив анонимный класс Runnable на лямбду. Теперь при выполнении параллельного потока, приложение не "зависает"

--- app/src/main/java/edu/mirea/onebeattrue/multitask/MyThread.kt
+++ app/src/main/java/edu/mirea/onebeattrue/multitask/MyThread.kt
@@ -2,10 +2,8 @@ package edu.mirea.onebeattrue.multitask
 
 class MyThread {
     fun doWork() {
-        Thread(object : Runnable {
-            override fun run() {
-                Thread.sleep(15000)
-            }
+        Thread(Runnable {
+            Thread.sleep(15000)
         }).start()
     }
 }
