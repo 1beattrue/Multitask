e9f1626d5bcdef2c7f69f82e5e533deda418b35b
Vitaly <onebeattrue@gmail.com>
2023-08-05 17:21:33 +0300

    Создание макета приложения для примера работы с потоками (Ввод имени - вывод имени)

---
+++ app/src/main/java/edu/mirea/onebeattrue/multitask/MainActivity.kt
@@ -0,0 +1,23 @@
+package edu.mirea.onebeattrue.multitask
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import edu.mirea.onebeattrue.multitask.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val saveButton = binding.saveButton
+        val editTextName = binding.editTextName
+        val textViewName = binding.textViewName
+
+        saveButton.setOnClickListener {
+            textViewName.text = editTextName.text.toString()
+        }
+    }
+}
